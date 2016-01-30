4c910f94a6463ae81063361a52f5b6a0bd6ef861
prabhat awasthi <koolboyprabhat@gmail.com>
2016-01-30 11:02:10 -0800

    example app files added

---
+++ app/src/main/java/com/podcopic/animationlib/androidsimplifiedanimation/Views/RotationOutView.java
@@ -0,0 +1,15 @@
+package com.podcopic.animationlib.androidsimplifiedanimation.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.podcopic.animationlib.androidsimplifiedanimation.R;
+
+public class RotationOutView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rotation_out_view);
+    }
+}
---
+++ app/src/main/java/com/podcopic/animationlib/androidsimplifiedanimation/Views/ZoomOutView.java
@@ -0,0 +1,15 @@
+package com.podcopic.animationlib.androidsimplifiedanimation.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.podcopic.animationlib.androidsimplifiedanimation.R;
+
+public class ZoomOutView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zoom_out_view);
+    }
+}
