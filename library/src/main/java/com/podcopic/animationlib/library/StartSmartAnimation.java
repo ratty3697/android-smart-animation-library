702e6e80c1b17bdeba5f3974693b8179dfd4fd01
ratty3697 <koolboyprabhat@gmail.com>
2016-08-19 22:22:54 +0530

    pivot bug fixed

--- library/src/main/java/com/podcopic/animationlib/library/StartSmartAnimation.java
+++ library/src/main/java/com/podcopic/animationlib/library/StartSmartAnimation.java
@@ -1,15 +1,13 @@
 package com.podcopic.animationlib.library;
 
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 
 /**
  * Created by hacker_ratty on 1/30/2016.
  */
 public class StartSmartAnimation {
-
-    static float w;
-    static float h;
 
     public StartSmartAnimation(){
 
@@ -22,36 +20,39 @@ public class StartSmartAnimation {
     public static void startAnimation(final View v , final AnimationType tech , final long duration ,
                                       final long delay , final boolean alpha){
 
+        final float[] w = new float[1];
+        final float[] h = new float[1];
+
         if(v.getWidth() == 0 || v.getHeight() == 0){//if view is just initialized for first time
             //delay is because view takes time to load its length
             Handler delayHandler = new Handler();
             delayHandler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    w = v.getWidth() / 2.0f;
-                    h = v.getHeight() / 2.0f;
+                    w[0] = v.getWidth() / 2.0f;
+                    h[0] = v.getHeight() / 2.0f;
 
                     SmartAnimation.with(tech)
                             .duration(duration)
                             .delay(delay)
                             .alpha(alpha)
-                            .horizontalCenter(w)
-                            .verticalCenter(h)
+                            .horizontalCenter(w[0])
+                            .verticalCenter(h[0])
                             .playOn(v);
                 }
-            },100);
+            },200);
 
 
         }
         else {//if view is alreaddy loaded
-            final float w = v.getWidth() / 2.0f;
-            final float h = v.getHeight() / 2.0f;
+            w[0] = v.getWidth() / 2.0f;
+            h[0] = v.getHeight() / 2.0f;
                 SmartAnimation.with(tech)
                         .duration(duration)
                         .delay(delay)
                         .alpha(alpha)
-                        .horizontalCenter(w)
-                        .verticalCenter(h)
+                        .horizontalCenter(w[0])
+                        .verticalCenter(h[0])
                         .playOn(v);
 
         }
@@ -65,6 +66,8 @@ public class StartSmartAnimation {
     public static void startAnimation(final View v , final AnimationType tech , final long duration ,
                                final long delay ,final boolean alpha ,final int slideLength){
 
+        final float[] w = new float[1];
+        final float[] h = new float[1];
         if(v.getWidth() == 0 || v.getHeight() == 0){//if view is just initialized for first time
 
             //delay is because view takes time to load its length
@@ -72,30 +75,30 @@ public class StartSmartAnimation {
             delayHandler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    w = v.getWidth() / 2.0f;
-                    h = v.getHeight() / 2.0f;
+                    w[0] = v.getWidth() / 2.0f;
+                    h[0] = v.getHeight() / 2.0f;
 
                     SmartAnimation.with(tech)
                             .duration(duration)
                             .delay(delay)
                             .alpha(alpha)
                             .slideLength(slideLength)
-                            .horizontalCenter(w)
-                            .verticalCenter(h)
+                            .horizontalCenter(w[0])
+                            .verticalCenter(h[0])
                             .playOn(v);
                 }
-            }, 100);
+            }, 250);
 
         }
         else {//if view is alreaddy loaded
-            final float w = v.getWidth() / 2.0f;
-            final float h = v.getHeight() / 2.0f;
+            w[0] = v.getWidth() / 2.0f;
+            h[0] = v.getHeight() / 2.0f;
             SmartAnimation.with(tech)
                     .duration(duration)
                     .delay(delay)
                     .slideLength(slideLength)
-                    .horizontalCenter(w)
-                    .verticalCenter(h)
+                    .horizontalCenter(w[0])
+                    .verticalCenter(h[0])
                     .playOn(v);
         }
 
